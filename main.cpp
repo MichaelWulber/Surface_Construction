5c8d95fc0fe8c8e2d25cb120749d410ccf7699de
Michael Wulber <mwulber2015@ufl.edu>
2018-04-25 16:35:19 -0400

    submitted version

--- main.cpp
+++ main.cpp
@@ -25,9 +25,9 @@
 
 std::vector<GLfloat> genMesh(ImplicitFunc* function, GLfloat cubeSize);
 
-std::vector<GLfloat> genCTMesh(std::vector<std::vector<std::vector<int>>> frames, double aspect_ratio);
+std::vector<GLfloat> genCTMesh(const std::vector<std::vector<std::vector<int>>> &vLuminance, const std::vector<std::vector<std::vector<int>>> &vCondition, double aspect_ratio);
 GLfloat CTinterpolate(GLfloat a, GLfloat b);
-std::vector<GLfloat> findVerticesCT(int i, int j, int k, int index, GLfloat* vertex[3]);
+std::vector<GLfloat> findVerticesCT(int i, int j, int k, int index, GLfloat* vertex[3], const std::vector<std::vector<std::vector<int>>> &vLuminance);
 
 int edgeListIndex(const bool arr[8]);
 std::vector<GLfloat> findVertices(int i, int j, int k, int index, GLfloat* vertex[3], GLfloat*** vals);
@@ -155,13 +155,14 @@ int main() {
 	const int dimX = 64;
 	const int dimY = 64;
 	const int numFrames = 64;
-	float frame_scale = 729/numFrames;
+	float frame_scale = 729 / numFrames;
 	int start = 0;
 
-	std::vector<std::vector<std::vector<int>>> frames(numFrames, std::vector<std::vector<int>>(dimX, std::vector<int>(dimY, 0)));
+	std::vector<std::vector<std::vector<int>>> vLuminance(numFrames, std::vector<std::vector<int>>(dimX, std::vector<int>(dimY, 0)));
+	std::vector<std::vector<std::vector<int>>> vCondition(numFrames, std::vector<std::vector<int>>(dimX, std::vector<int>(dimY, 0)));
 	for (int index = 1; index <= numFrames; ++index) {
 		// Load DICOM file
-		std::unique_ptr<imebra::DataSet> loadedDataSet(imebra::CodecFactory::load( getFilePath(index * frame_scale + start) ));
+		std::unique_ptr<imebra::DataSet> loadedDataSet(imebra::CodecFactory::load(getFilePath(index * frame_scale + start)));
 
 		// Get Image
 		std::unique_ptr<imebra::Image> image(loadedDataSet->getImageApplyModalityTransform(0));
@@ -179,22 +180,24 @@ int main() {
 		float y_factor = (float)height / (float)dimY;
 		for (int i = 0; i < width; ++i) {
 			for (int j = 0; j < height; ++j) {
-				// For monochrome images
-
 				std::int32_t luminance = dataHandler->getSignedLong(j * width + i);
+				vLuminance[index - 1][i / x_factor][j / y_factor] = luminance;
 
-				if (luminance > 100 && luminance < 900) {
-					frames[index - 1][i / x_factor][j / y_factor] = 1;
+				if (luminance > 0) {
+					vCondition[index - 1][i / x_factor][j / y_factor] = 1;
+				}
+				else {
+					vCondition[index - 1][i / x_factor][j / y_factor] = 0;
 				}
 			}
 		}
-		
+
 		std::cout << index << std::endl;
 	}
 	std::cout << "finished" << std::endl;
 	// --- end ---
 
-	std::vector<GLfloat> CTVertices = genCTMesh(frames, 1.25);
+	std::vector<GLfloat> CTVertices = genCTMesh(vLuminance, vCondition, 1.25);
 	ct = Mesh(0.95f, 0.52f, 0.16f);
 	ct.setVPositions(CTVertices);
 	ct.genVNormals();
@@ -579,7 +582,7 @@ std::string getFilePath(int index) {
 
 	if (index < 10) {
 		return path + "00" + std::to_string(index) + type;
-	} 
+	}
 
 	else if (index < 100) {
 		return path + "0" + std::to_string(index) + type;
@@ -590,7 +593,7 @@ std::string getFilePath(int index) {
 	}
 }
 
-std::vector<GLfloat> genCTMesh(std::vector<std::vector<std::vector<int>>> vertices, double aspect_ratio) {
+std::vector<GLfloat> genCTMesh(const std::vector<std::vector<std::vector<int>>> &vLuminance, const std::vector<std::vector<std::vector<int>>> &vCondition, double aspect_ratio) {
 	std::cout << "generating mesh..." << std::endl;
 	GLfloat minX = -1.0f * aspect_ratio;
 	GLfloat minY = -1.0f;
@@ -601,9 +604,9 @@ std::vector<GLfloat> genCTMesh(std::vector<std::vector<std::vector<int>>> vertic
 	GLfloat x, y, z, a;
 	bool byteArray[8];
 
-	const GLint dimX = vertices.size();
-	const GLint dimY = vertices[0].size();
-	const GLint dimZ = vertices[0][0].size();
+	const GLint dimX = vCondition.size();
+	const GLint dimY = vCondition[0].size();
+	const GLint dimZ = vCondition[0][0].size();
 
 	GLfloat* vertexCoord[3] = { new GLfloat[dimX], new GLfloat[dimY], new GLfloat[dimZ] };
 	for (GLint i = 0; i < dimX; ++i) {
@@ -624,6 +627,18 @@ std::vector<GLfloat> genCTMesh(std::vector<std::vector<std::vector<int>>> vertic
 		vertexCoord[2][i] = z;
 	}
 
+	//for (GLint i = 0; i < dimX - 1; ++i) {
+	//	for (GLint j = 0; j < dimY - 1; ++j) {
+	//		if (vCondition[i][j][dimZ / 2] == 1 && vLuminance[i][j][dimZ / 2] < 100) {
+	//			std::cout << "( " << vCondition[i][j][dimZ / 2] << ", " << vLuminance[i][j][dimZ / 2] << " )"<< std::endl;
+	//		}
+
+	//		else if (vCondition[i][j][dimZ / 2] == 0 && vLuminance[i][j][dimZ / 2] > 100) {
+	//			std::cout << "( " << vCondition[i][j][dimZ / 2] << ", " << vLuminance[i][j][dimZ / 2] << " )" << std::endl;
+	//		}
+	//	}
+	//}
+
 	// Go through every cube and check vertices;
 	// triangleVertices stores vertices of facets as {x0, y0, z0, x1, y1, z1, ..., xn, yn, zn}
 	std::vector<GLfloat> triangleVertices;
@@ -631,17 +646,17 @@ std::vector<GLfloat> genCTMesh(std::vector<std::vector<std::vector<int>>> vertic
 	for (GLint i = 0; i < dimX - 1; ++i) {
 		for (GLint j = 0; j < dimY - 1; ++j) {
 			for (GLint k = 0; k < dimZ - 1; ++k) {
-				byteArray[0] = vertices[i][j][k];
-				byteArray[1] = vertices[i + 1][j][k];
-				byteArray[2] = vertices[i + 1][j][k + 1];
-				byteArray[3] = vertices[i][j][k + 1];
-				byteArray[4] = vertices[i][j + 1][k];
-				byteArray[5] = vertices[i + 1][j + 1][k];
-				byteArray[6] = vertices[i + 1][j + 1][k + 1];
-				byteArray[7] = vertices[i][j + 1][k + 1];
+				byteArray[0] = vCondition[i][j][k];
+				byteArray[1] = vCondition[i + 1][j][k];
+				byteArray[2] = vCondition[i + 1][j][k + 1];
+				byteArray[3] = vCondition[i][j][k + 1];
+				byteArray[4] = vCondition[i][j + 1][k];
+				byteArray[5] = vCondition[i + 1][j + 1][k];
+				byteArray[6] = vCondition[i + 1][j + 1][k + 1];
+				byteArray[7] = vCondition[i][j + 1][k + 1];
 				int index = edgeListIndex(byteArray);
 
-				temp = findVerticesCT(i, j, k, index, vertexCoord);
+				temp = findVerticesCT(i, j, k, index, vertexCoord, vLuminance);
 				triangleVertices.insert(triangleVertices.end(), temp.begin(), temp.end());
 			}
 		}
@@ -651,11 +666,11 @@ std::vector<GLfloat> genCTMesh(std::vector<std::vector<std::vector<int>>> vertic
 	return triangleVertices;
 }
 
-GLfloat CTinterpolate(GLfloat a, GLfloat b) {
-	return (a + b) / 2;
+GLfloat CTinterpolateA(GLfloat a, GLfloat aVal, GLfloat b, GLfloat bVal) {
+	return a + ((0 - aVal) * (b - a) / (bVal - aVal));
 }
 
-std::vector<GLfloat> findVerticesCT(int i, int j, int k, int index,GLfloat* vertex[3]) {
+std::vector<GLfloat> findVerticesCT(int i, int j, int k, int index, GLfloat* vertex[3], const std::vector<std::vector<std::vector<int>>> &vLuminance) {
 	std::vector<GLfloat> triangleVertices;
 	int edgeNum;
 	GLfloat intersection;
@@ -675,8 +690,11 @@ std::vector<GLfloat> findVerticesCT(int i, int j, int k, int index,GLfloat* vert
 			z = vertex[2][k];
 
 			a = vertex[0][i];
+			aVal = vLuminance[i][j][k];
 			b = vertex[0][i + 1];
-			intersection = CTinterpolate(a, b);
+			bVal = vLuminance[i + 1][j][k];
+
+			intersection = interpolate(a, aVal, b, bVal);
 
 			triangleVertices.push_back(intersection);
 			triangleVertices.push_back(y);
@@ -687,8 +705,11 @@ std::vector<GLfloat> findVerticesCT(int i, int j, int k, int index,GLfloat* vert
 			y = vertex[1][j];
 
 			a = vertex[2][k];
+			aVal = vLuminance[i + 1][j][k];
 			b = vertex[2][k + 1];
-			intersection = CTinterpolate(a, b);
+			bVal = vLuminance[i + 1][j][k + 1];
+
+			intersection = interpolate(a, aVal, b, bVal);
 
 			triangleVertices.push_back(x);
 			triangleVertices.push_back(y);
@@ -699,8 +720,11 @@ std::vector<GLfloat> findVerticesCT(int i, int j, int k, int index,GLfloat* vert
 			z = vertex[2][k + 1];
 
 			a = vertex[0][i];
+			aVal = vLuminance[i][j][k + 1];
 			b = vertex[0][i + 1];
-			intersection = CTinterpolate(a, b);
+			bVal = vLuminance[i + 1][j][k + 1];
+
+			intersection = CTinterpolateA(a, aVal, b, bVal);
 
 			triangleVertices.push_back(intersection);
 			triangleVertices.push_back(y);
@@ -711,8 +735,11 @@ std::vector<GLfloat> findVerticesCT(int i, int j, int k, int index,GLfloat* vert
 			y = vertex[1][j];
 
 			a = vertex[2][k];
+			aVal = vLuminance[i][j][k];
 			b = vertex[2][k + 1];
-			intersection = CTinterpolate(a, b);
+			bVal = vLuminance[i][j][k + 1];
+
+			intersection = interpolate(a, aVal, b, bVal);
 
 			triangleVertices.push_back(x);
 			triangleVertices.push_back(y);
@@ -723,8 +750,11 @@ std::vector<GLfloat> findVerticesCT(int i, int j, int k, int index,GLfloat* vert
 			z = vertex[2][k];
 
 			a = vertex[0][i];
+			aVal = vLuminance[i][j + 1][k];
 			b = vertex[0][i + 1];
-			intersection = CTinterpolate(a, b);
+			bVal = vLuminance[i + 1][j + 1][k];
+
+			intersection = CTinterpolateA(a, aVal, b, bVal);
 
 			triangleVertices.push_back(intersection);
 			triangleVertices.push_back(y);
@@ -735,8 +765,11 @@ std::vector<GLfloat> findVerticesCT(int i, int j, int k, int index,GLfloat* vert
 			y = vertex[1][j + 1];
 
 			a = vertex[2][k];
+			aVal = vLuminance[i + 1][j + 1][k];
 			b = vertex[2][k + 1];
-			intersection = CTinterpolate(a, b);
+			bVal = vLuminance[i + 1][j + 1][k + 1];
+
+			intersection = CTinterpolateA(a, aVal, b, bVal);
 
 			triangleVertices.push_back(x);
 			triangleVertices.push_back(y);
@@ -747,8 +780,11 @@ std::vector<GLfloat> findVerticesCT(int i, int j, int k, int index,GLfloat* vert
 			z = vertex[2][k + 1];
 
 			a = vertex[0][i];
+			aVal = vLuminance[i][j + 1][k + 1];
 			b = vertex[0][i + 1];
-			intersection = CTinterpolate(a, b);
+			bVal = vLuminance[i + 1][j + 1][k + 1];
+
+			intersection = interpolate(a, aVal, b, bVal);
 
 			triangleVertices.push_back(intersection);
 			triangleVertices.push_back(y);
@@ -759,8 +795,11 @@ std::vector<GLfloat> findVerticesCT(int i, int j, int k, int index,GLfloat* vert
 			y = vertex[1][j + 1];
 
 			a = vertex[2][k];
+			aVal = vLuminance[i][j + 1][k];
 			b = vertex[2][k + 1];
-			intersection = CTinterpolate(a, b);
+			bVal = vLuminance[i][j + 1][k + 1];
+
+			intersection = interpolate(a, aVal, b, bVal);
 
 			triangleVertices.push_back(x);
 			triangleVertices.push_back(y);
@@ -771,8 +810,11 @@ std::vector<GLfloat> findVerticesCT(int i, int j, int k, int index,GLfloat* vert
 			z = vertex[2][k];
 
 			a = vertex[1][j];
+			aVal = vLuminance[i][j][k];
 			b = vertex[1][j + 1];
-			intersection = CTinterpolate(a, b);
+			bVal = vLuminance[i][j + 1][k];
+
+			intersection = interpolate(a, aVal, b, bVal);
 
 			triangleVertices.push_back(x);
 			triangleVertices.push_back(intersection);
@@ -783,8 +825,11 @@ std::vector<GLfloat> findVerticesCT(int i, int j, int k, int index,GLfloat* vert
 			z = vertex[2][k];
 
 			a = vertex[1][j];
+			aVal = vLuminance[i + 1][j][k];
 			b = vertex[1][j + 1];
-			intersection = CTinterpolate(a, b);
+			bVal = vLuminance[i + 1][j + 1][k];
+
+			intersection = interpolate(a, aVal, b, bVal);
 
 			triangleVertices.push_back(x);
 			triangleVertices.push_back(intersection);
@@ -795,8 +840,11 @@ std::vector<GLfloat> findVerticesCT(int i, int j, int k, int index,GLfloat* vert
 			z = vertex[2][k + 1];
 
 			a = vertex[1][j];
+			aVal = vLuminance[i + 1][j][k + 1];
 			b = vertex[1][j + 1];
-			intersection = CTinterpolate(a, b);
+			bVal = vLuminance[i + 1][j + 1][k + 1];
+
+			intersection = interpolate(a, aVal, b, bVal);
 
 			triangleVertices.push_back(x);
 			triangleVertices.push_back(intersection);
@@ -807,8 +855,11 @@ std::vector<GLfloat> findVerticesCT(int i, int j, int k, int index,GLfloat* vert
 			z = vertex[2][k + 1];
 
 			a = vertex[1][j];
+			aVal = vLuminance[i][j][k + 1];
 			b = vertex[1][j + 1];
-			intersection = CTinterpolate(a, b);
+			bVal = vLuminance[i][j + 1][k + 1];
+
+			intersection = interpolate(a, aVal, b, bVal);
 
 			triangleVertices.push_back(x);
 			triangleVertices.push_back(intersection);
